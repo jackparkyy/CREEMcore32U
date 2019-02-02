578caceb83f88c754ad07d8d5095b1e00cac18f8
Jack Parkinson <jackparkyy@gmail.com>
2019-02-02 16:37:30 +0000

    ADD more test vectors to immediate decoder testbench

--- testbenches/Immediate_Decoder_testbench.vhd
+++ testbenches/Immediate_Decoder_testbench.vhd
@@ -23,7 +23,7 @@ begin
 		imm => imm
 	);
 	
-	simulation : process
+	process
 		procedure test(
 			constant inst_type : in string;
 			constant opcode : std_logic_vector(6 downto 2);
@@ -38,9 +38,10 @@ begin
 			wait for time_delta;
 
 			res := conv_integer(imm);
+
 			assert res = expected
-			report "Unexpected result: " &
-			"instruction type = " & inst_type & "; " &
+			report "unexpected result: " &
+			"instruction_type = " & inst_type & "; " &
 			"opcode = " & to_string(opcode) & "; " &
 			"result = " & integer'image(res) & "; " &
 			"expected = " & integer'image(expected)
@@ -51,7 +52,7 @@ begin
 			constant immediate : in std_logic_vector(31 downto 20);
 			constant expected : in integer
 		) is
-			constant inst_type 	: string := "I";
+			constant inst_type 	: string := "i";
 			variable opcode 	: std_logic_vector(6 downto 2);
 			variable inst		: std_logic_vector(31 downto 2);
 		begin
@@ -59,12 +60,92 @@ begin
 			inst := immediate & (19 downto 7 => '1') & opcode;
 			test(inst_type, opcode, inst, expected);
 			opcode := "11001";
+			inst := immediate & (19 downto 7 => '1') & opcode;
 			test(inst_type, opcode, inst, expected);
 			opcode := "00000";
+			inst := immediate & (19 downto 7 => '1') & opcode;
 			test(inst_type, opcode, inst, expected);
 		end procedure test_itype;
+
+		procedure test_utype(
+			constant immediate : in std_logic_vector(31 downto 12);
+			constant expected : in std_logic_vector(31 downto 0)
+		) is
+			constant inst_type 	: string := "u";
+			variable opcode 	: std_logic_vector(6 downto 2);
+			variable inst		: std_logic_vector(31 downto 2);
+		begin
+			opcode := "01101";
+			inst := immediate & (11 downto 7 => '1') & opcode;
+			test(inst_type, opcode, inst, conv_integer(expected));
+			opcode := "00101";
+			inst := immediate & (11 downto 7 => '1') & opcode;
+			test(inst_type, opcode, inst, conv_integer(expected));
+		end procedure test_utype;
+
+		procedure test_jtype(
+			constant immediate : in std_logic_vector(31 downto 12);
+			constant expected : in std_logic_vector(31 downto 0)
+		) is
+			constant inst_type 	: string := "j";
+			variable opcode 	: std_logic_vector(6 downto 2);
+			variable inst		: std_logic_vector(31 downto 2);
+		begin
+			opcode := "11011";
+			inst := immediate & (11 downto 7 => '1') & opcode;
+			test(inst_type, opcode, inst, conv_integer(expected));
+		end procedure test_jtype;
+
+		procedure test_btype(
+			constant immediate : in std_logic_vector(11 downto 0);
+			constant expected : in std_logic_vector(31 downto 0)
+		) is
+			constant inst_type 	: string := "b";
+			variable opcode 	: std_logic_vector(6 downto 2);
+			variable inst		: std_logic_vector(31 downto 2);
+		begin
+			opcode := "11000";
+			inst := immediate(11 downto 5) & (24 downto 12 => '1') & immediate(4 downto 0) & opcode;
+			test(inst_type, opcode, inst, conv_integer(expected));
+		end procedure test_btype;
+
+		procedure test_stype(
+			constant immediate : in std_logic_vector(11 downto 0);
+			constant expected : in integer
+		) is
+			constant inst_type 	: string := "s";
+			variable opcode 	: std_logic_vector(6 downto 2);
+			variable inst		: std_logic_vector(31 downto 2);
+		begin
+			opcode := "01000";
+			inst := immediate(11 downto 5) & (24 downto 12 => '1') & immediate(4 downto 0) & opcode;
+			test(inst_type, opcode, inst, expected);
+		end procedure test_stype;
 	begin
-		test_itype("000000000100", 5);
+		test_itype(x"000", 0);
+		test_itype(x"aaa", -1366);
+		test_itype(x"555", 1365);
+		test_itype(x"fff", -1);
+
+		test_utype(x"00000", x"00000000");
+		test_utype(x"aaaaa", x"aaaaa000");
+		test_utype(x"55555", x"55555000");
+		test_utype(x"fffff", x"fffff000");
+
+		test_jtype(x"00000", x"00000000");
+		test_jtype(x"aaaaa", x"fffaa2aa");
+		test_jtype(x"55555", x"00055d54");
+		test_jtype(x"fffff", x"fffffffe");
+
+		test_btype(x"000", x"00000000");
+		test_btype(x"aaa", x"fffff2aa");
+		test_btype(x"555", x"00000d54");
+		test_btype(x"fff", x"fffffffe");
+
+		test_stype(x"000", 0);
+		test_stype(x"aaa", -1366);
+		test_stype(x"555", 1365);
+		test_stype(x"fff", -1);
 		wait;
-	end process simulation;
+	end process;
 end behaviour;
