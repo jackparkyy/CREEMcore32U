7655fb5e2e71aafd847bae3679c2f8763a770bb6
Jack Parkinson <jackparkyy@gmail.com>
2019-02-02 16:34:32 +0000

    REMOVE old files

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # CREEMcore32U
 
 Compact RISC-V Extensible Embedded Microprocessor core 32-Bit Unfinished
-
-## Setup with Quartus Prime
-add the following line to `simulation/modelsim/modelsim.ini` riscv = 
-```
-../../libraries/riscv
-```
--- libraries/riscv/rv32i/rv32i_types.vhd
+++
@@ -1,8 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-
-package rv32i_types is
-    subtype word is std_logic_vector(31 downto 0);
-    subtype half_word is std_logic_vector(15 downto 0);
-    subtype byte is std_logic_vector(7 downto 0);
-end package rv32i_types;
