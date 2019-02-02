5dcb8159e8836ba419bf9558adbdbe493ab94c6d
Jack Parkinson <jackparkyy@gmail.com>
2019-02-02 17:17:17 +0000

    CLEAN immediate decoder code

--- 32U_core/U32_Immediate_Decoder.vhd
+++ 32U_core/U32_Immediate_Decoder.vhd
@@ -17,8 +17,7 @@ begin
     opcode <= inst(6 downto 2); -- isolate opcode from instrcution
     
     with opcode select 
-    imm <=  inst(31 downto 12) & (11 downto 0 => '0')                                               when lui, -- U-type
-            inst(31 downto 12) & (11 downto 0 => '0')                                               when auipc, -- U-type
+    imm <=  inst(31 downto 12) & (11 downto 0 => '0')                                               when lui | auipc, -- U-type
             (31 downto 20 => inst(31)) & inst(19 downto 12) & inst(20) & inst(30 downto 21) & '0'   when jal, -- J-type
             (31 downto 12 => inst(31)) & inst(7) & inst(30 downto 25) & inst(11 downto 8) & '0'     when branch, -- B-type
             (31 downto 11 => inst(31)) & inst(30 downto 25) & inst(11 downto 7)                     when store, -- S-type
