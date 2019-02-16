aa1b676e889509a4f2f4f5e2d412abcbac4180f0
Jack Parkinson <jackparkyy@gmail.com>
2019-02-16 22:24:28 +0000

    UPDATE data memory
    allow the size of memory to be set with single constant

--- 32U_core/U32_Data_Memory.vhd
+++ 32U_core/U32_Data_Memory.vhd
@@ -13,51 +13,53 @@ entity u32_data_memory is
 end u32_data_memory;
     
 architecture behavioral of u32_data_memory is
-    type data_mem is array (0 to 255) of byte_vector;
+    type byte_ram is array (0 to (data_mem_size - 1)) of byte_vector;
 
-    signal ram_addr : byte_vector := (others => '0');
-    signal ram      : data_mem := (others => (others => '0'));
+    subtype byte_addr is natural range 0 to (data_mem_size - 1);
+
+    signal ram_addr : byte_addr := 0;
+    signal data_ram : byte_ram := (others => (others => '0'));
     signal data     : word_vector := (others => '0');
 
-    constant third_last_byte    : byte_vector := x"FD";
-    constant second_last_byte   : byte_vector := x"FE";
-    constant last_byte          : byte_vector := x"FF";
+    constant third_last_byte    : byte_addr := (data_mem_size - 3);
+    constant second_last_byte   : byte_addr := (data_mem_size - 2);
+    constant last_byte          : byte_addr := (data_mem_size - 1);
 
     constant byte           : std_logic_vector(1 downto 0) := "00";
     constant half_word      : std_logic_vector(1 downto 0) := "01";
     constant word           : std_logic_vector(1 downto 0) := "10";
 begin
-    ram_addr <= addr(7 downto 0);
+    ram_addr <= to_integer(unsigned(addr));
 
-    data <= x"000000"   & ram(to_integer(unsigned(ram_addr)))       when ram_addr = last_byte else
-            x"0000"     & ram(to_integer(unsigned(ram_addr)) + 1)
-                        & ram(to_integer(unsigned(ram_addr)))       when ram_addr = second_last_byte else
-            x"00"       & ram(to_integer(unsigned(ram_addr)) + 2)
-                        & ram(to_integer(unsigned(ram_addr)) + 1)
-                        & ram(to_integer(unsigned(ram_addr)))       when ram_addr = third_last_byte else
-            ram(to_integer(unsigned(ram_addr)) + 3) &
-            ram(to_integer(unsigned(ram_addr)) + 2) &
-            ram(to_integer(unsigned(ram_addr)) + 1) &
-            ram(to_integer(unsigned(ram_addr)));
+    data <= x"000000"   & data_ram(ram_addr)       when ram_addr = last_byte else
+            x"0000"     & data_ram(ram_addr + 1)
+                        & data_ram(ram_addr)       when ram_addr = second_last_byte else
+            x"00"       & data_ram(ram_addr + 2)
+                        & data_ram(ram_addr + 1)
+                        & data_ram(ram_addr)       when ram_addr = third_last_byte else
+            data_ram(ram_addr + 3) &
+            data_ram(ram_addr + 2) &
+            data_ram(ram_addr + 1) &
+            data_ram(ram_addr);
 
     read_data <= data when (mem_read = '1') else x"00000000";
 
     process begin
         wait until rising_edge(clk);
         if (mem_write = '1' and funct = word and ram_addr /= last_byte and ram_addr /= second_last_byte and ram_addr /= third_last_byte) then
-            ram(to_integer(unsigned(ram_addr))) <= write_data(7 downto 0);
-            ram(to_integer(unsigned(ram_addr)) + 1) <= write_data(15 downto 8);
-            ram(to_integer(unsigned(ram_addr)) + 2) <= write_data(23 downto 16);
-            ram(to_integer(unsigned(ram_addr)) + 3) <= write_data(31 downto 24);
+            data_ram(ram_addr) <= write_data(7 downto 0);
+            data_ram(ram_addr + 1) <= write_data(15 downto 8);
+            data_ram(ram_addr + 2) <= write_data(23 downto 16);
+            data_ram(ram_addr + 3) <= write_data(31 downto 24);
         elsif (mem_write = '1' and funct = word and ram_addr /= last_byte and ram_addr /= second_last_byte) then
-            ram(to_integer(unsigned(ram_addr))) <= write_data(7 downto 0);
-            ram(to_integer(unsigned(ram_addr)) + 1) <= write_data(15 downto 8);
-            ram(to_integer(unsigned(ram_addr)) + 2) <= write_data(23 downto 16);
+            data_ram(ram_addr) <= write_data(7 downto 0);
+            data_ram(ram_addr + 1) <= write_data(15 downto 8);
+            data_ram(ram_addr + 2) <= write_data(23 downto 16);
         elsif (mem_write = '1' and funct /= byte and ram_addr /= last_byte) then
-            ram(to_integer(unsigned(ram_addr))) <= write_data(7 downto 0);
-            ram(to_integer(unsigned(ram_addr)) + 1) <= write_data(15 downto 8);
+            data_ram(ram_addr) <= write_data(7 downto 0);
+            data_ram(ram_addr + 1) <= write_data(15 downto 8);
         elsif (mem_write = '1') then
-            ram(to_integer(unsigned(ram_addr))) <= write_data(7 downto 0);
+            data_ram(ram_addr) <= write_data(7 downto 0);
         end if;
     end process;
 end behavioral;
