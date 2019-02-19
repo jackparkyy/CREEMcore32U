184709973e1cd127d80d571acb3dcb32417ccad5
Jack Parkinson <jackparkyy@gmail.com>
2019-02-19 15:49:39 +0000

    COMBINE inst memory and inst pipeline stage

--- 32U_core/U32_Inst_Fetch.vhd
+++ 32U_core/U32_Inst_Fetch.vhd
@@ -1,44 +1,43 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 use work.u32_types.all;
 
 entity u32_inst_fetch is
     port (
         clk, write_en, pc_src           : in std_logic      := '0';
         write_inst, write_addr, new_pc  : in word_vector    := (others => '0');
-        inst, pc_out, next_pc_out       : out word_vector   := (others => '0');
-        clk_out                         : out std_logic     := '0'
+        inst, pc_out, next_pc_out       : out word_vector   := (others => '0')
     );
 end u32_inst_fetch;
     
 architecture rtl of u32_inst_fetch is
-    signal pc_reg_in, pc_reg_out, inst_reg  : word_vector   := (others => '0');
-    signal clock                            : std_logic      := '0';
+    constant increment      : std_logic_vector(xlen downto 0)   := ((xlen downto 1 => '0') & '1');
+    constant inst_mem_xlen  : natural                           := ((inst_mem_size / 4) - 1);
 
-    constant increment  : std_logic_vector(xlen downto 0)   := ((xlen downto 1 => '0') & '1');
+    type ram is array (0 to inst_mem_xlen) of word_vector;
+
+    signal pc_reg_in, pc_reg_out, inst_reg  : word_vector   := (others => '0');
+    signal inst_ram                         : ram           := (others => (others => '0'));
 begin
-    u32_instruction_memory : entity work.u32_instruction_memory
-	port map (
-        clk => clk,
-        write_en => write_en,
-        read_addr => pc_reg_out,
-        write_inst => write_inst,
-        write_addr => write_addr,
-        inst => inst_reg,
-        clk_out => clock
-    );
+    -- concurrent statements (read)
+    inst_reg <= inst_ram(to_integer(unsigned(pc_reg_out)));
 
     pc_reg_in <=    new_pc when pc_src = '1' else
                     pc_reg_out + increment;
 
-    clk_out <= clock;
     -- sequential statements
-    process begin
-        wait until falling_edge(clock);
-        pc_reg_out <= pc_reg_in;
-        pc_out <= pc_reg_out;
-        next_pc_out <= pc_reg_out + increment;
-        inst <= inst_reg;
+    process (clk) begin
+        if rising_edge(clk) then
+            if write_en = '1' then
+                inst_ram(to_integer(unsigned(write_addr))) <= write_inst;
+            end if;
+        elsif falling_edge(clk) then
+            pc_reg_out <= pc_reg_in;
+            pc_out <= pc_reg_out;
+            next_pc_out <= pc_reg_out + increment;
+            inst <= inst_reg;
+        end if;
     end process;
 end rtl;
